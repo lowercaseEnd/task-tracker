e56ee6bd3e0c1d6153bc1efcfa0372e6d4aedb2b
John Doe <randomEme@protonmail.com>
2020-03-31 15:02:13 +0400

    Добавил фильтр по статусу

--- src/views/TasksView.vue
+++ src/views/TasksView.vue
@@ -1,8 +1,19 @@
 <template>
   <section>
+    <b-form>
+      <b-form-group>
+        <b-form-select v-model="selected">
+          <b-form-select-option value="all">all</b-form-select-option>
+          <b-form-select-option value="completed">completed</b-form-select-option>
+          <b-form-select-option value="not completed">not completed</b-form-select-option>
+        </b-form-select>
+      </b-form-group>
+    </b-form>
     <ul class="tasks-list">
-      <task-item v-for="item in tasks" :key="item.id" :task="item" />
+      
+      <task-item  v-for="item in select" :key="item.id" :task="item" />
     </ul>
+
   </section>
 </template>
 
@@ -12,11 +23,25 @@ const TaskItem = () => import("@/components/TaskItem");
 export default {
   data() {
     return {
-      tasks: JSON.parse(localStorage.getItem("tasks"))
+      tasks: JSON.parse(localStorage.getItem("tasks")),
+      selected: "all"
     }
   },
   components: {
     TaskItem
+  },
+  computed: {
+    select() {
+      if(this.selected === "completed") {
+        return this.tasks.filter(item => item.completed === true);
+      }
+      if(this.selected === "not completed") {
+        return this.tasks.filter(item => item.completed === false);
+      }
+      if(this.selected === "all") {
+        return this.tasks;
+      }
+    }
   }
 }
 </script>
