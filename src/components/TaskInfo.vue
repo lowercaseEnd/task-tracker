6a33b00e85537822d073768581f8964b360b0c01
John Doe <randomEme@protonmail.com>
2020-03-31 13:12:44 +0400

    Создал компонент для ввода данных

---
+++ src/components/TaskInfo.vue
@@ -0,0 +1,46 @@
+<template>
+  <section>
+    <b-form @submit.prevent>
+      <legend>Создание задачи</legend>
+      <b-form-group label="Task name">
+        <b-form-input v-model="name" type="text" id="task-name" placeholder="Название" required />
+      </b-form-group>
+      <b-form-group label="Task tags">
+        <b-form-tags v-model="tags" type="text" id="task-tags" remove-on-delete/>
+      </b-form-group>
+      <b-form-group label="Task description">
+        <b-form-textarea v-model="description" name id="task-description" cols="30" rows="10">Описание</b-form-textarea>
+      </b-form-group>
+      <b-form-group label="Pick date">
+        <b-form-datepicker id="task-date" v-model="date" />
+        <p>{{date}}</p>
+      </b-form-group>
+      <b-button @click="saveTask">Сохранить</b-button>
+    </b-form>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "",
+      tags: [],
+      description: "",
+      date: ""
+    }
+  },
+  methods: {
+    saveTask() {
+      let temp = JSON.parse(localStorage.getItem("tasks")) || [];
+      temp.push({
+        name: this.name,
+        tags: this.tags,
+        description: this.description,
+        date: this.date
+      });
+      localStorage.setItem("tasks", JSON.stringify(temp));
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <task-info />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import TaskInfo from "@/components/TaskInfo";
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    TaskInfo
   }
 }
 </script>
